0e53c3bda3fe1facc5145b16a827e0224bf2e9b7
kailastr <mailtokailastr@gmail.com>
2024-02-25 23:59:47 +0530

    Restuarant Cart section

--- client/src/components/Cart/CartContainer.jsx
+++ client/src/components/Cart/CartContainer.jsx
@@ -1,8 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoCloseSharp } from 'react-icons/io5';
+import { IoMdArrowDropup, IoMdArrowDropright } from 'react-icons/io'
+import { useNavigate } from 'react-router-dom';
+
+//component
+import FoodItem from './FoodItem';
+
+const CartData = ({ toggle }) => {
+
+    const [cart, setCart] = useState([
+        {
+            image: "https://b.zmtcdn.com/data/dish_photos/44f/bc817ecae2106a8afad4d6f22be7c44f.jpg",
+            name: "Chicken Mandhi Spicy",
+            rating: 4.5,
+            price: 200,
+            description: "Chicken mandhi spicy is a flavourful Saudi Arabian rice dish made with tender chicken marinated in aromatic spices",
+            quantity: 2,
+            totalPrice: 400
+        },
+        {
+            image: "https://b.zmtcdn.com/data/dish_photos/5f3/426d684ea6b0f7edc2cd44119a0e55f3.jpg",
+            name: "Al Faham Mandi Quarter",
+            rating: 3.5,
+            price: 250,
+            description: "Chicken slow cooked in a fiery blend of chilli powder, coriander powder and a freshly-ground red chilli paste.",
+            quantity: 3,
+            totalPrice: 750
+        }
+    ]);
+
+    const navigate = useNavigate();
+
+    const continueToCheckout = () => navigate('/checkout/orders');
+
+    return (
+        <>
+            <div className='flex items-center justify-start gap-5 p-2 rounded-md bg-zomato-50'>
+                <div className='flex flex-col items-start'>
+                    <small className='flex items-center gap-1 cursor-pointer' onClick={toggle}>
+                        {cart.length} Items <IoMdArrowDropup />
+                    </small>
+                    {/* reduce function to combine the total price of the products in the cart */}
+                    <h4>₹ {cart.reduce((acc, curVal) => acc + curVal.totalPrice, 0)} <small>(+ Tax)</small> </h4>
+                </div>
+                <button
+                    onClick={continueToCheckout}
+                    className='flex items-center gap-1 bg-zomato-400 px-3 py-1 text-white rounded-lg'
+                >
+                    Continue <IoMdArrowDropright />
+                </button>
+            </div>
+        </>
+    )
+};
 
 const CartContainer = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [cart, setCart] = useState([
+        {
+            image: "https://b.zmtcdn.com/data/dish_photos/44f/bc817ecae2106a8afad4d6f22be7c44f.jpg",
+            name: "Chicken Mandhi Spicy",
+            rating: 4.5,
+            price: 200,
+            description: "Chicken mandhi spicy is a flavourful Saudi Arabian rice dish made with tender chicken marinated in aromatic spices",
+            quantity: 2,
+            totalPrice: 400
+        },
+        {
+            image: "https://b.zmtcdn.com/data/dish_photos/5f3/426d684ea6b0f7edc2cd44119a0e55f3.jpg",
+            name: "Al Faham Mandi Quarter",
+            rating: 3.5,
+            price: 250,
+            description: "Chicken slow cooked in a fiery blend of chilli powder, coriander powder and a freshly-ground red chilli paste.",
+            quantity: 3,
+            totalPrice: 750
+        }
+    ]);
+
+    const toggleCart = () => setIsOpen((prev) => !prev);
+    const closeCart = () => setIsOpen(false);
     return (
-        <div>CartContainer</div>
+        <>
+            {/* if cart have any elements in it, then only show the cart section */}
+            {cart.length &&
+                <>
+                    {isOpen && (
+                        <div className='flex fixed w-full overflow-y-scroll h-48 bg-white z-10 p-2 bottom-14 px-3'>
+                            <div className='flex items-center justify-between md:px-20'>
+                                <h3 className='text-xl font-semibold'>Your Orders</h3>
+                                <IoCloseSharp onClick={closeCart} />
+                            </div>
+
+                            <hr className='my-2' />
+
+                            <div className='flex flex-col gap-2 md:px-20'>
+                                {cart.map((food) => (
+                                    <FoodItem key={food._id} {...food} />
+                                ))}
+                            </div>
+                        </div>
+                    )}
+
+                    <div className='fixed w-full bg-white z-10 p-2 bottom-0 ms-auto lg:px-20'>
+                        <CartData onClick={toggleCart} />
+                    </div>
+                </>
+            }
+        </>
     )
 }
 
---
+++ client/src/components/Cart/FoodItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FoodItem = () => {
+    return (
+        <div>FoodItem</div>
+    )
+}
+
+export default FoodItem
