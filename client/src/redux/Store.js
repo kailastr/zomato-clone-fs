94569389aca17bdaec3b12b9b1277738a6166d21
kailastr <mailtokailastr@gmail.com>
2024-02-29 22:39:24 +0530

    Initial setup, Store and rootReducer setup of Redux

---
+++ client/src/redux/Store.js
@@ -0,0 +1,19 @@
+//main entry point or storage of redux
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers/rootReducer';
+
+//redux middleware
+const middleware = [thunk];
+
+// to check if we are on the development phase or Notification. We want to use redux only if it is on the development phase.Notification
+if (process.env.NODE_ENV === "development") {
+    // const { Logger, default: logger } = require("redux-logger");
+    const { logger } = require("redux-logger");
+    middleware.push(logger);
+}
+
+const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+
+export default store;
---
+++ client/src/redux/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+//to combine all the storage units
+import { combineReducers } from 'redux';
+
+//reducers or storage units
+const rootReducer = combineReducers({});
+
+export default rootReducer;
